32f1c2029af291f064d6bf81992d08d428709345
Daniel Vedin <dv222bk@student.lnu.se>
2015-09-18 19:52:31 +0200

    DateTime
    Added datetime string

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -5,7 +5,7 @@ class DateTimeView {
 
 	public function show() {
 
-		$timeString = 'TODO, Write servertime here...';
+		$timeString = date("l") . ', the ' . date("jS \of F Y") . ', The time is ' . date("H:i:s");
 
 		return '<p>' . $timeString . '</p>';
 	}
