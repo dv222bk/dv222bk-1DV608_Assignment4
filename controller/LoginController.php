535e094fcdd0de243513296e117f4d149f0adba4
Daniel Vedin <dv222bk@student.lnu.se>
2015-09-23 16:05:18 +0200

    Final comments, error reporting
    Added the final comments to the code. Also removed error reporting since
    it's no longer needed.

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -17,10 +17,10 @@
 		 */
 		public function loginUser() {
 			if($this->view->sessionsExists()) {
-				if($this->view->checkSessionUserAgent()) {
+				if($this->view->checkSessionUserAgent()) { // check if the session user is in fact the real session owner
 					$this->model->login($this->view->getSessionName(), $this->view->getSessionPassword());
 				} else {
-					$this->model->login('', '');
+					$this->model->login('', ''); // login using empty strings to force a fail login
 				}
 			} else if ($this->view->cookiesExists()) {
 				$this->model->login($this->view->getCookieName(), $this->view->getCookiePassword());
--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -4,10 +4,12 @@ namespace view;
 
 class DateTimeView {
 
+	/**
+	 * Create and return a date string with the current date
+	 * @return The current date in a html string
+	 */
 	public function show() {
-
 		$timeString = date("l") . ', the ' . date("jS \of F Y") . ', The time is ' . date("H:i:s");
-
 		return '<p>' . $timeString . '</p>';
 	}
 }
